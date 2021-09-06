ad00d0d53ea7ea873673625fcac45304f57a5a03
AbbasHaiderAbidi <abbashaider2131995@gmail.com>
2021-09-06 17:05:05 +0530

    newloa_html

--- psdf_main/user_payment.py
+++ psdf_main/user_payment.py
@@ -7,12 +7,12 @@ def new_loa(request):
         if request.method == 'POST':
             req = request.POST
             projid = req.get('projectid')
-            print(projid)
-            aboq = boqdata.objects.filter(project = projects.objects.get(id = projid), boqtype = '2')
+            thisproject = projects.objects.get(id = projid)
+            aboq = boqdata.objects.filter(project = thisproject, boqtype = '2')
             context['aboq'] = aboq
             context['approved_boq_total'] = boq_grandtotal(aboq)
-            
-        
+            context['loa_project'] = thisproject
+            return render(request, 'psdf_main/_user_new_loa.html', context)
         userobj = users.objects.filter(username = request.session['user'])[:1].get()
         print(userobj)
         context['projectlist'] = projects.objects.filter(status = '4', userid = userobj)
